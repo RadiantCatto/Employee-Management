186a328dee0901844d1dc4fd61913fedfe5809dd
RadiantCatto <reymarvillahermosa1415@gmail.com>
2023-04-17 12:05:37 +0800

    Update views.py

--- mysite/Users/views.py
+++ mysite/Users/views.py
@@ -6,31 +6,37 @@ from django.core.exceptions import ValidationError
 from .models import Users
 from .serializers import UsersSerializer
 
+# Define a class-based view to create a new user
 class CreateUserView(APIView):
+
+    # Define the serializer class to use
     serializer_class = UsersSerializer
 
+    # Define the POST method to handle creating a new user
     def post(self, request, format=None):
+
+        # Retrieve the necessary data from the request
         employee_id = request.data.get('employee_id')
         useraccess = request.data.get('useraccess')
         passphrase = request.data.get('passphrase')
 
-        # Check if employee already has an account
+        # Check if the employee already has an account
         if Users.objects.filter(employee_id=employee_id).exists():
             return Response({'error': 'Employee already has an account'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Check if username already exists
+        # Check if the username already exists
         if Users.objects.filter(useraccess=useraccess).exists():
             return Response({'error': 'Username already in use'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Validate password format
+        # Validate password format using a separate method
         if not is_valid_password(passphrase):
             return Response({'error': 'Invalid password format'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Generate salt and hash password
+        # Generate a salt and hash the password
         salt = generate_salt()
         hashed_passphrase = make_password(passphrase, salt=salt)
 
-        # Create user record
+        # Create a new user record
         data = {'employee_id': employee_id, 'useraccess': useraccess, 'passphrase': hashed_passphrase, 'salt': salt}
         serializer = UsersSerializer(data=data)
         if serializer.is_valid():
@@ -38,10 +44,12 @@ class CreateUserView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    # Define a method to validate the password format
     def is_valid_password(password):
         # Implement password validation logic here
         return True
 
+    # Define a method to generate a random salt
     def generate_salt():
         # Implement salt generation logic here
         return 'randomsalt'
