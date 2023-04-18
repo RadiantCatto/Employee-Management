5faddda01c516f9897cbd49573fe34b8e7451feb
RadiantCatto <reymarvillahermosa1415@gmail.com>
2023-04-18 17:20:17 +0800

    Update views.py
    import uuid
    import hashlib

--- mysite/Users/views.py
+++ mysite/Users/views.py
@@ -1,55 +1,32 @@
+import uuid
+import hashlib
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth.hashers import make_password
-from django.core.exceptions import ValidationError
-from .models import Users
 from .serializers import UsersSerializer
 
-# Define a class-based view to create a new user
-class CreateUserView(APIView):
+class CreateUserAPIView(APIView):
+    def post(self, request):
+        serializer = UsersSerializer(data=request.data)
 
-    # Define the serializer class to use
-    serializer_class = UsersSerializer
-
-    # Define the POST method to handle creating a new user
-    def post(self, request, format=None):
-
-        # Retrieve the necessary data from the request
-        employee_id = request.data.get('employee_id')
-        useraccess = request.data.get('useraccess')
-        passphrase = request.data.get('passphrase')
-
-        # Check if the employee already has an account
-        if Users.objects.filter(employee_id=employee_id).exists():
-            return Response({'error': 'Employee already has an account'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Check if the username already exists
-        if Users.objects.filter(useraccess=useraccess).exists():
-            return Response({'error': 'Username already in use'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Validate password format using a separate method
-        if not is_valid_password(passphrase):
-            return Response({'error': 'Invalid password format'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Generate a salt and hash the password
-        salt = generate_salt()
-        hashed_passphrase = make_password(passphrase, salt=salt)
-
-        # Create a new user record
-        data = {'employee_id': employee_id, 'useraccess': useraccess, 'passphrase': hashed_passphrase, 'salt': salt}
-        serializer = UsersSerializer(data=data)
         if serializer.is_valid():
-            serializer.save(created_by=request.user.username)
+            # Generate a unique salt for each record
+            salt = uuid.uuid4().hex
+
+            # Hash the plain text password with the generated salt
+            password = serializer.validated_data['passphrase']
+            # Encode the salted password as UTF-8 and hash it using SHA-512
+            hashed_password = hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
+
+            # Save the new user record to the database
+            user = serializer.save(salt=salt, passphrase=hashed_password)
+
+            # Return a response with the created user's employee_id, useraccess, and employee_id
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            errors = dict(serializer.errors)
+            if 'non_field_errors' in errors:
+                del errors['non_field_errors']
+            # Return a response with the serializer's validation errors
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # Define a method to validate the password format
-    def is_valid_password(password):
-        # Implement password validation logic here
-        return True
-
-    # Define a method to generate a random salt
-    def generate_salt():
-        # Implement salt generation logic here
-        return 'randomsalt'
