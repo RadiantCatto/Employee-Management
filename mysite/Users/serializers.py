6853863b3837330293eecccf1ceee3457438c151
RadiantCatto <reymarvillahermosa1415@gmail.com>
2023-04-18 17:19:40 +0800

    Update serializers.py

--- mysite/Users/serializers.py
+++ mysite/Users/serializers.py
@@ -1,12 +1,43 @@
+
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
-from .models import Users
+from .models import Users, Employees
 
 
-# Define a serializer for the Users model, inheriting from Django REST Framework's ModelSerializer class
-class UsersSerializer(serializers.ModelSerializer):
-    # Define a Meta class to provide additional options for the serializer
+class EmployeesSerializer(serializers.ModelSerializer):
     class Meta:
-        # Set the model for the serializer to the Users model
+        model = Employees
+        fields = ( 'firstname', 'middlename', 'lastname',
+                   'suffix', 'birthday', 'civilstatus',
+                  'created_date', 'updated_date',  'isRegular',
+                  'RegularizationDate', 'EmploymentDate')
+
+class UsersSerializer(serializers.ModelSerializer):
+    employee_id = serializers.PrimaryKeyRelatedField(queryset=Employees.objects.all())
+
+    class Meta:
         model = Users
-        # Specify that all fields in the model should be included in the serialized output
-        fields = '__all__'
+        fields = ('id', 'employee_id', 'useraccess', 'passphrase', 'created_by')
+
+    def create(self, validated_data):
+        employee_id = validated_data.pop('employee_id')
+        user = Users.objects.create(employee_id=employee_id, **validated_data)
+        return user
+
+    def validate_employee_id(self, value):
+        # Check if employee_id already exists in users table
+        if Users.objects.filter(employee_id=value).exists():
+            raise serializers.ValidationError("Employee already has an account")
+        return value
+    
+    def validate_useraccess(self, value):
+        # Check if useraccess value is unique
+        if Users.objects.filter(useraccess=value).exists():
+            raise serializers.ValidationError("Username already in use")
+        return value
+
+    def validate_passphrase(self, value):
+        # Perform custom password validation using Django's built-in password validation and third-party library django-password-validators
+        validate_password(value)  # check length, common sequences, and other built-in validation rules
+        return value
