61779fef88a1dd41e8b0adfea5b8f2ae5491cfb7
RadiantCatto <reymarvillahermosa1415@gmail.com>
2023-04-22 19:03:29 +0800

    update urls.py

--- mysite/Users/urls.py
+++ mysite/Users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateUserAPIView,UsersListView
+from .views import CreateUserAPIView,UsersListView,UserLoginAPIView
 
 urlpatterns = [
-    path('create-user/', CreateUserAPIView.as_view(), name='create-user'),
-    path('users/', UsersListView.as_view(), name='users-list'),
+    path('users/create-user/', CreateUserAPIView.as_view(), name='create-user'),
+    path('users/list/', UsersListView.as_view(), name='users-list'),
+    path('users/login/', UserLoginAPIView.as_view(), name='login')
 ]
